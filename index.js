d512c17acb72e5291b08790ab0e840e6f04876ef
Rimon Moomey <rimon.moomey@gmail.com>
2022-10-14 14:44:00 -0500

    complete moveDodgerRight

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.left = `${right + 1}px`;
+  }
+}
